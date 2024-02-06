12e40cfed9a9436e3bc7314da80fbfa5ac77e097
Himath Ranaweera <145730950+HimathRanaweera@users.noreply.github.com>
2024-02-06 22:19:16 +0530

    add a button for  DashboardForm.fxml

--- Pos_System/src/main/java/Controller/DashboardFormController.java
+++ Pos_System/src/main/java/Controller/DashboardFormController.java
@@ -1,4 +1,28 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class DashboardFormController {
+
+    public Button categoryButton;
+    public Button ItemsButton;
+    public Button OrdersButton;
+    public Button ReportsButton;
+
+    public void categoryButtonOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void ItemsButtonOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void OrdersButtonOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void ReportsButtonOnAction(ActionEvent actionEvent) {
+
+    }
 }
