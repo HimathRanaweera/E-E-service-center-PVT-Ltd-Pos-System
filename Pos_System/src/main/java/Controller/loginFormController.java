36f589b6551683cde5b77b5b8c831b5d12c7970b
Himath Ranaweera <145730950+HimathRanaweera@users.noreply.github.com>
2024-02-06 19:06:46 +0530

    DashboardForm.fxml name change the LoginForm.fxml

---
+++ Pos_System/src/main/java/Controller/loginFormController.java
@@ -0,0 +1,38 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class loginFormController {
+    public Button loginButton;
+    public BorderPane pane;
+    public TextField userNameTextField;
+    public PasswordField userNamePasswordTextField;
+
+    public void loginButtonOnAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/CustomerForm.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void userNameTextFieldOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void userNamePasswordTextFieldOnAction(ActionEvent actionEvent) {
+
+    }
+}
